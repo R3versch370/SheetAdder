1d2c0aabca2f016246e24959ff1b31fa49c976cc
R3versch370 <michelle.kuang@hotmail.com>
2021-11-01 01:42:10 +1100

    Adding ability to add to spreadsheet

--- main.py
+++ main.py
@@ -65,6 +65,23 @@ results.append({
             }
             }})
 
+def update_doc_with_guess(date, number, colour, user, officialFlag):
+
+    credentials =service_account.Credentials.from_service_account_file(TOKEN_FILE, scopes=SCOPES)
+    service = build('sheets', 'v4', credentials=credentials)
+    data = service.spreadsheets().values().get(spreadsheetId=DOCUMENT_ID, range="A:A", majorDimension="ROWS").execute()
+    if not officialFlag:
+        range_row = find_row_number(data, date, user)
+        if range_row is None:
+            return None
+        document = service.spreadsheets().values().update(spreadsheetId=DOCUMENT_ID, range=range_row, valueInputOption="USER_ENTERED",body={"values":[[int(number)],[colour]], "majorDimension": "COLUMNS"}).execute()
+    else:
+        dateRow = find_row_number(data, date, "official")
+        range_row = f"C{dateRow}:D{dateRow}"
+        document = service.spreadsheets().values().update(spreadsheetId=DOCUMENT_ID, range=range_row, valueInputOption="USER_ENTERED",body={"values":[[int(number)],[colour]], "majorDimension": "COLUMNS"}).execute()
+        find_cells_to_highlight(number, colour, dateRow, service)
+    return "Success"
+
 @client.event
 async def on_ready():
 
@@ -76,4 +93,35 @@ async def on_message(message):
     if message.content.startswith('Hello Bot'):
 
         await message.channel.send(f'Hello <@{message.author.id}>!')
+	if message.content.startswith('!glady'):
 
+        date = datetime.datetime.utcnow()
+        if date.hour >= 1:
+            date = datetime.datetime.now() + datetime.timedelta(days=1)
+        print(date)
+        guess = message.content.split(" ",1)
+        number, colour = guess[1].split(" ", 1)
+        number = re.sub(r"[^0-9 ]+", "", number)
+        colour = re.sub(r"[^A-Za-z ]+", "", colour)
+
+        user = message.author
+        response = update_doc_with_guess(date, number, colour, user, False)
+        if response is None:
+            await message.channel.send(f"Failed to send guess")
+        else:
+            await message.channel.send(f"Sent case guesses {number} {colour} to excel")
+
+    elif message.content.startswith('!official'):
+
+        date = datetime.datetime.utcnow()
+        if date.hour < 1:
+            date = datetime.datetime.now() - datetime.timedelta(days=1)
+
+        guess = message.content.split(" ",1)
+        number, colour = guess[1].split(" ", 1)
+        number = re.sub(r"[^0-9 ]+", "", number)
+        colour = re.sub(r"[^A-Za-z ]+", "", colour)
+
+        update_doc_with_guess(date, number, colour, None, True)
+
+        await message.channel.send(f"Updated official data")
